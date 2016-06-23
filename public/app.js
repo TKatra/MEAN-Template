f5d7e2043194451e4686618eb10b9ddf1c553a8c
TKatra <tobiaskatra@hotmail.com>
2016-06-23 13:53:04 +0200

    Removed comments.

--- public/app.js
+++ public/app.js
@@ -1,8 +1,6 @@
 'use strict';
 
 angular.module(appConfig.name, appConfig.dependencies);
-// angular.module('app', ['ui.router', 'main']);
-// angular.module('app', ['ui.router']);
 
 angular.module(appConfig.name).config(['$locationProvider', function($locationProvider) {
   $locationProvider.html5Mode(true);
